859d5ef5cc9e1ca3650bf1d3aa50d84b0677ff5a
Luyee <abc5542211@gmail.com>
2022-06-17 11:18:18 +0800

    噪音計Ok

--- sensor/connect.py
+++ sensor/connect.py
@@ -9,10 +9,10 @@ def getaDate():
     # Value1-6校正後的值 Value14-19校正前的值
     DATA = sensor.Sensor()
     AF_DATA = {}
-    
+
     # Value1-7校正後 Value14-20校正前
     comparison = {'Value1': 'Value14', 'Value2': 'Value15', 'Value3': 'Value16',
-                  'Value4': 'Value17', 'Value5': 'Value18', 'Value6': 'Value19', 'Value7':'Value20'}
+                  'Value4': 'Value17', 'Value5': 'Value18', 'Value6': 'Value19', 'Value7': 'Value20'}
 
     for af, be in comparison.items():
         search = DBmysql.read_mysql(
@@ -24,9 +24,11 @@ def getaDate():
 
     return DATA
 
+
 def getInitial():
     INITIAL = DBmysql.read_mysql("REVISE", "select * from INITIAL;")[0]
     return INITIAL
+
 
 def cal_N(Noise):  # 第1個數字-Value1
     data = []
@@ -36,10 +38,12 @@ def cal_N(Noise):  # 第1個數字-Value1
     cal = 10*math.log(sum(data)*(1/len(Noise)), 10)
     return cal
 
-def All_mean(ago,aft):
+
+def All_mean(ago, aft):
     DATA = {}
     INITIAL = getInitial()
-    range_data = DBmysql.read_mysql("SENSOR", ("select Value1,Value2,Value3,Value4,Value5,Value6,Value7 from SENSOR_DB where Time >= '{}' and Time < '{}';").format(ago,aft))
+    range_data = DBmysql.read_mysql(
+        "SENSOR", ("select Value1,Value2,Value3,Value4,Value5,Value6,Value7 from SENSOR_DB where Time >= '{}' and Time < '{}';").format(ago, aft))
     DATA['Proj_ID'] = INITIAL[1]
     DATA['STID'] = INITIAL[2]
     DATA['Value1'] = cal_N(range_data)
@@ -56,18 +60,19 @@ def All_mean(ago,aft):
 class time_judge:
     def __init__(self):
         self.second = None
-    def range_second(self,timestamps,second):
+
+    def range_second(self, timestamps, second):
         s = timestamps
         if self.second != s:
-            if s% second ==0:
+            if s % second == 0:
                 self.second = s
                 return True
         return False
-    def cal_time(self,now,second,fmt):
+
+    def cal_time(self, now, second, fmt):
         ago = now+datetime.timedelta(seconds=second)
         AGO = datetime.datetime.strftime(ago, fmt)
         return ago
-
 
 
 judge_T001 = time_judge()
@@ -78,33 +83,31 @@ judge_T60 = time_judge()
 while(1):
     now = datetime.datetime.now()
     timstamps = int(now.timestamp())
-    if judge_T001.range_second(timstamps,3) :
+    if judge_T001.range_second(timstamps, 3):
         DATA = getaDate()
         DBmysql.write_mysql("SENSOR", "SENSOR_DB", DATA)
-    if judge_T01.range_second(timstamps,60) :
-        ago = judge_T01.cal_time(now,-60,'%Y-%m-%d %H:%M:00')
-        aft = judge_T01.cal_time(now,0,'%Y-%m-%d %H:%M:00')
-        DATA = All_mean(ago,aft)
+    if judge_T01.range_second(timstamps, 60):
+        ago = judge_T01.cal_time(now, -60, '%Y-%m-%d %H:%M:00')
+        aft = judge_T01.cal_time(now, 0, '%Y-%m-%d %H:%M:00')
+        DATA = All_mean(ago, aft)
         DATA['Time'] = ago
         DBmysql.write_mysql("SENSOR", "T01", DATA)
-        print("T01",ago,aft)
-    if judge_T05.range_second(timstamps,300) :
-        ago = judge_T01.cal_time(now,-300,'%Y-%m-%d %H:%M:00')
-        aft = judge_T01.cal_time(now,0,'%Y-%m-%d %H:%M:00')
-        DATA = All_mean(ago,aft)
+        print("T01", ago, aft)
+    if judge_T05.range_second(timstamps, 300):
+        ago = judge_T01.cal_time(now, -300, '%Y-%m-%d %H:%M:00')
+        aft = judge_T01.cal_time(now, 0, '%Y-%m-%d %H:%M:00')
+        DATA = All_mean(ago, aft)
         DATA['Time'] = ago
         DBmysql.write_mysql("SENSOR", "T05", DATA)
-        print("T05",ago,aft)
-    if judge_T60.range_second(timstamps,3600) :
-        ago = judge_T01.cal_time(now,-3600,'%Y-%m-%d %H:00:00')
-        aft = judge_T01.cal_time(now,0,'%Y-%m-%d %H:00:00')
-        DATA = All_mean(ago,aft)
+        print("T05", ago, aft)
+    if judge_T60.range_second(timstamps, 3600):
+        ago = judge_T01.cal_time(now, -3600, '%Y-%m-%d %H:00:00')
+        aft = judge_T01.cal_time(now, 0, '%Y-%m-%d %H:00:00')
+        DATA = All_mean(ago, aft)
         DATA['Time'] = ago
         DBmysql.write_mysql("SENSOR", "T60", DATA)
-        print("T60",ago,aft)
+        print("T60", ago, aft)
 
 '''
 print(getaDate())
    '''
-
-
--- sensor/sendsocket.py
+++ sensor/sendsocket.py
@@ -5,90 +5,96 @@ from datetime import date
 import datetime
 import DBmysql
 
+
 class time_judge:
     def __init__(self):
         self.second = None
-    def range_second(self,timestamps,second):
+
+    def range_second(self, timestamps, second):
         s = timestamps
         if self.second != s:
-            if s% second ==0:
+            if s % second == 0:
                 self.second = s
                 return True
         return False
 
 
 def connected():
-    i=0
-    while i!=1:
+    i = 0
+    while i != 1:
         try:
-            i=1
+            i = 1
             initial = getinitial()
-            IP=initial[0][6]
-            PORT=initial[0][7]
+            IP = initial[0][6]
+            PORT = initial[0][7]
             ADDR = (IP, int(PORT))
-            client.connect(ADDR)# 用來請求連接遠程服務器
+            client.connect(ADDR)  # 用來請求連接遠程服務器
             print("[successful connected]")
             return True
-            
+
         except:
             print("[!cannot connect server or no wifi]")
-            i=0
+            i = 0
             time.sleep(5)
-        
+
+
 def getinitial():
-    initial=DBmysql.read_mysql("REVISE","select * from `INITIAL`")
+    initial = DBmysql.read_mysql("REVISE", "select * from `INITIAL`")
     return initial
 
-def getData(TABLE):#getData('SENSOR_DB')
-    data={}
-    value = DBmysql.read_mysql("SENSOR",("SELECT * FROM `{}` ORDER BY `ID` DESC LIMIT 0,1 ").format(TABLE))[0]
-    column_name = DBmysql.read_mysql_column_name("SENSOR",("SELECT * FROM `{}` ORDER BY `ID` DESC LIMIT 0,1 ").format(TABLE))
-    for index,item in enumerate(column_name):
-        data[item]=value[index]
-    data['Time']=str(data['Time'])
+
+def getData(TABLE):  # getData('SENSOR_DB')
+    data = {}
+    value = DBmysql.read_mysql(
+        "SENSOR", ("SELECT * FROM `{}` ORDER BY `ID` DESC LIMIT 0,1 ").format(TABLE))[0]
+    column_name = DBmysql.read_mysql_column_name(
+        "SENSOR", ("SELECT * FROM `{}` ORDER BY `ID` DESC LIMIT 0,1 ").format(TABLE))
+    for index, item in enumerate(column_name):
+        data[item] = value[index]
+    data['Time'] = str(data['Time'])
     return data
+
 
 def send(msg):
     try:
         initial = getinitial()
-        IP=initial[0][6]
-        PORT=initial[0][7]
+        IP = initial[0][6]
+        PORT = initial[0][7]
         ADDR = (IP, int(PORT))
-        client.sendto(msg.encode('utf-8'),ADDR)
+        client.sendto(msg.encode('utf-8'), ADDR)
     except:
         print("[!send fail!] retest after 5seconds")
         time.sleep(5)
-        
+
+
 def goEPA(TABLE):
     try:
         all_data = getData(TABLE)
-        all_data['TABLE']=TABLE
+        all_data['TABLE'] = TABLE
         send(str(all_data))
-        print(TABLE,all_data['Time'])
+        print(TABLE, all_data['Time'])
     except:
         print("retest mysocket after 30seconds")
         time.sleep(30)
-        
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET:默認IPv4, SOCK_STREAM:UDP
+# AF_INET:默認IPv4, SOCK_STREAM:UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 judge_T001 = time_judge()
 judge_T01 = time_judge()
 judge_T05 = time_judge()
 judge_T60 = time_judge()
 
-if connected() == True :
+if connected() == True:
     while(1):
         now = datetime.datetime.now()
         timstamps = int(now.timestamp())
-        if judge_T001.range_second(timstamps,2) :
+        if judge_T001.range_second(timstamps, 2):
             goEPA("SENSOR_DB")
-        if judge_T01.range_second(timstamps,60) :
+        if judge_T01.range_second(timstamps, 60):
             goEPA("T01")
-        if judge_T05.range_second(timstamps,300) :
+        if judge_T05.range_second(timstamps, 300):
             goEPA("T05")
-        if judge_T60.range_second(timstamps,3600) :
+        if judge_T60.range_second(timstamps, 3600):
             goEPA("T60")
-
-
